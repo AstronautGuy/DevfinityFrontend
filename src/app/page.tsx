73cf3a99a3a746f5bd89691f2a3f56c2abd789fa
TheAstronautGuy <theastronautguy2@gmail.com>
2026-06-19 19:44:22 +0530

    feat: integrate CoreCapabilitiesSection into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
+import { CoreCapabilitiesSection } from "@/components/CoreCapabilitiesSection";
 import { FeaturedProjectsSection } from "@/components/FeaturedProjectsSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
+      <CoreCapabilitiesSection />
       <AboutSection />
       <FeaturedProjectsSection />
     </div>
