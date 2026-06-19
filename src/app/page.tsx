15e0a01ad0a1f1706f6e1a2dec6235561c1d67d8
TheAstronautGuy <theastronautguy2@gmail.com>
2026-06-19 19:37:20 +0530

    feat: integrate landing page sections into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
+import { FeaturedProjectsSection } from "@/components/FeaturedProjectsSection";
+
 export default function Home() {
   return (
-      <main>
-      </main>
+    <div className="flex flex-col min-h-screen">
+      <HeroSection />
+      <AboutSection />
+      <FeaturedProjectsSection />
+    </div>
   );
 }
